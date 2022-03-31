49126f808ee8b456d2a89c79e98fb6658b38563d
Raissa Azevedo <rhaii.azevedo@gmail.com>
2022-03-31 10:40:15 -0300

    CRUD genérico

---
+++ cursos/views1.py
@@ -0,0 +1,34 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+
+class CursoAPIView(APIView):
+    ''' API DE CURSOS '''
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"msg": "Curso criado com sucesso"}, status=status.HTTP_201_CREATED)
+
+
+class AvaliacaoAPIView(APIView):
+    '''API DE AVALIAÇÕES'''
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AvaliacaoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
